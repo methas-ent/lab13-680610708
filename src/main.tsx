564ffad72a65b184e042108424cbee11597f25d2
Methas <160356559+methas-ent@users.noreply.github.com>
2026-09-08 22:13:46 +0700

    "edited import bootstrap"

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
